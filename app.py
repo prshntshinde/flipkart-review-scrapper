e4c095cbaed6ee691b3a63149ea44b8c55941513
prashant <mr.pshinde@uipath.com>
2023-07-25 18:01:37 +0530

    feat: Import required packages.

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import logging
+logging.basicConfig(filename="logs/scrapper.log", level=logging.INFO)
