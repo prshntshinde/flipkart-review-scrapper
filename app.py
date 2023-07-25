4475f5c779fff403eb23eb1ac9029811fa984a86
prashant <mr.pshinde@uipath.com>
2023-07-25 18:47:43 +0530

    feat: Flask setup

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="logs/scrapper.log", level=logging.INFO)
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
